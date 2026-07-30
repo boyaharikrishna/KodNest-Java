7b31b174384abf9ef3e6cb21204a94ab8e48d1b1
Hari Krishna <boyaharikrishna552@gamil.com>
2026-07-30 12:45:14 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome to the java Track");
+        System.out.println("I Have Already know how to build programming logic code");
+        System.out.println("Now i am now i am learing how to express that logic using java");
+
     }
 }
