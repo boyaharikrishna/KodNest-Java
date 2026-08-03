fe5453efb6d381d0681361427a42c1e8ec3f3b59
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-03 21:07:59 +0530

    Day2changes

---
+++ track/M02-Introduction-to-java/T01-Introcution-java-and-programming/M01-T01-ST01-first-Program/work/progressReport.java
@@ -0,0 +1,17 @@
+
+public class progressReport {
+
+    public static void main(String[] args) {
+        int completedTopics = 17;
+        int TotalTopics = 20;
+        int Dailylearninghours = 3;
+        int Learningdays = 5;
+        int remaining = TotalTopics - completedTopics;
+        int weeklylearningHours = Learningdays * Dailylearninghours;
+        double progressPercentage = completedTopics * 100 / TotalTopics;
+        System.out.println("Completed Topics: " + completedTopics);
+        System.out.println("Remaining Topics: " + remaining);
+        System.out.println("Weekly Learning Hours: " + weeklylearningHours);
+        System.out.println("Progress Percentagee: " + progressPercentage);
+    }
+}
