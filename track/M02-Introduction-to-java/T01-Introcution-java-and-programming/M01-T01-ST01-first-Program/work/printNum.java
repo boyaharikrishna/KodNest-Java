c607d2e7f53e5ca08730d72944d14cf9ddcc4f89
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-04 12:27:47 +0530

    Day3changes

---
+++ track/M02-Introduction-to-java/T01-Introcution-java-and-programming/M01-T01-ST01-first-Program/work/printNum.java
@@ -0,0 +1,14 @@
+
+public class printNum {
+
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println("Postive");
+        } else if (num == 0) {
+            System.out.println("zero");
+        } else {
+            System.out.println("Negatie");
+        }
+    }
+}
