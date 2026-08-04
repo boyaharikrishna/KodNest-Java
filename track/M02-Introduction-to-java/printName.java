424e05de45f8354c050c626ac17c42b16a0dcb44
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-04 11:44:43 +0530

    Day3changes

---
+++ track/M02-Introduction-to-java/printName.java
@@ -0,0 +1,13 @@
+
+public class printName {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.print("Thursday");
+        System.out.println("Friday");
+        System.out.print("Saturday");
+        System.out.println("Sunday");
+    }
+}
