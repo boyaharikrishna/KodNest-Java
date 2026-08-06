e7eb7129f48dd54be5b608b0154c16c7055c280d
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-06 14:06:45 +0530

    Day5 changes

---
+++ track/practice/placementPractice.java
@@ -0,0 +1,15 @@
+public class placementPractice {
+    public static void main(String[] args) {
+        int marks = 68;
+        int attendance = 80;
+        int practiceDays = 3;
+
+        // Write your code here
+        String res = (marks >= 60 &&  attendance >=75) ? "Placement Ready" : "Continue Preparation";
+        System.out.println(res);
+        for(int i=1;i<=3;i++){
+            System.out.println("Practice Day: " + i);
+        }
+    }
+}
+
