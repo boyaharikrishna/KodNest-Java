e128cd90753a44030a3ce2a3fb471e0710cdc328
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-06 12:51:02 +0530

    Day5 changes

---
+++ track/M02-Introduction-to-java/T01-Introcution-java-and-programming/M02-T01-ST01-first-Program/M02-T01-ST01-control-flow-loops/usingBreakNested.java
@@ -0,0 +1,16 @@
+public class usingBreakNested {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (j == 2) {
+                    break;
+
+                }
+                System.out.println("i: " + i + " j: " + j);
+            }
+           
+        }
+         System.out.println();
+    }
+
+}
