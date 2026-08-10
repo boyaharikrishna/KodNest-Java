170b085d7c64803041bf83b7000089d7027124d2
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-10 13:11:09 +0530

    day7changes

---
+++ track/practice/using.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class using {
+    public static void main(String[] args) {
+        // int marks = 70;
+        // int attendence = 80;
+        // boolean projectStatus = true;
+        // boolean status = (marks  >= 60 && attendence >= 75 && projectStatus ) ? true : false;
+        // System.out.println(status);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String name =sc. nextLine();
+        double a = sc.nextDouble();
+
+System.out.println(n);
+System.out.println(name);
+System.out.println(a);
+    }
+}
