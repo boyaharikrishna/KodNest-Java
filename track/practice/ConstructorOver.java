dc6e6fc2867ebf5eddfe59121a8283daa21e7961
Hari Krishna <boyaharikrishna552@gamil.com>
2026-08-14 12:25:38 +0530

    day10changes

---
+++ track/practice/ConstructorOver.java
@@ -0,0 +1,39 @@
+
+class Emp {
+    String name;
+    int age;
+    double height;
+
+    Emp(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    Emp() {
+        name = "Hair";
+        age = 12;
+        height = 5.5;
+    }
+
+    Emp(String name) {
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+public class ConstructorOver {
+    public static void main(String[] args) {
+        Emp e1 = new Emp("Krsihna", 25, 2.2);
+        e1.display();
+        Emp e2 = new Emp("maha dev");
+        e2.display();
+        Emp e3 = new Emp();
+        e3.display();
+    }
+}
