83214b646a9adb035d213615bf606d556c58d9ab
Hari Krishna <boyaharikrishna552@gamil.com>
2026-09-06 01:19:09 +0530

    day14changes

---
+++ track/M02-Introduction-to-java/Arrays/SwapAdjcent.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class SwapAdjcent {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+       
+        for (int i = 0; i < arr.length; i+=2) {
+   
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+        }
+        for(int i=0;i<arr.length;i++){
+              System.out.print(arr[i] + " ");
+        }
+    }
+      
+} 
+
