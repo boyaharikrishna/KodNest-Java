a1994c80525939d958e11f37ae3c731cf4cfab31
Hari Krishna <boyaharikrishna552@gamil.com>
2026-09-08 00:30:34 +0530

    day16changes

---
+++ track/M02-Introduction-to-java/String/Pgm3.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Pgm3 {
+    public static void main(String[] args) {
+         String s5 = "Java";
+         String s6 = "Java";
+        if(s5.equals(s6)){
+            System.out.println("content are equal");
+        }else{
+            System.out.println("content are no equal");
+        }
+    }
+}
